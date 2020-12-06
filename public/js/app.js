897bd11144d99d2558e06fb1fa6bf1ddb7a7778f
Sang Nguyen <sangnn2012@gmail.com>
2020-12-06 13:51:56 +0700

    add message

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-fetch("http://puzzle.mead.io/puzzle").then(async (resp) => {
-  let data = await resp.json();
-  console.log(data);
-});
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector('#message-1');
@@ -14,11 +9,13 @@ messageTwo.textContent = '';
 weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const location = search.value;
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = '';
   fetch(`http://localhost:3000/weather?address=${location}`).then(async (resp) => {
     let data = await resp.json();
     if (data.error) {
-      messageTwo.textContent = data.error;
-      messageOne.textContent = '';
+      messageTwo.innerHTML = data.error;
+      messageOne.innerHTML = '';
     } else {
       let content = `Location: ${data.location}<br>Forecast: ${data.forecastData}`;
       messageOne.innerHTML = content;
