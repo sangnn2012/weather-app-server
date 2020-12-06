78ff9bf09c5b3e35736450a59fdb23f45cbe2fae
Sang Nguyen <sangnn2012@gmail.com>
2020-12-06 13:34:49 +0700

    fix bugs print to log instead of send to client. Print data to UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,18 +5,24 @@ fetch("http://puzzle.mead.io/puzzle").then(async (resp) => {
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+messageOne.textContent = '';
+messageTwo.textContent = '';
 
 weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const location = search.value;
-  console.log({ location });
-
   fetch(`http://localhost:3000/weather?address=${location}`).then(async (resp) => {
     let data = await resp.json();
     if (data.error) {
-      return console.log(err);
+      messageTwo.textContent = data.error;
+      messageOne.textContent = '';
+    } else {
+      let content = `Location: ${data.location}<br>Forecast: ${data.forecastData}`;
+      messageOne.innerHTML = content;
+      messageTwo.innerHTML = '';
     }
-    console.log(data.location);
-    console.log(data.forecastData);
   });
 });
